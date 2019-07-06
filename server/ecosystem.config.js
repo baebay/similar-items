8836c6ef087cb255c3a4a8176e7f8f34aee5b9c8
Doris Hui <dorishui225@gmail.com>
2019-07-06 12:25:42 -0500

    modified post deploy

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:baebay/similar-items.git',
       path: '/home/ubuntu/seabay',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'npm install && pm2 startOrRestart ~/seabay/source/server/ecosystem.config.js'
     }
   }
 }
