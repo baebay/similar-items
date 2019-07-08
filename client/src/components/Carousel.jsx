cb125a37888c6cda2b189226e26ffde04d89305c
Doris Hui <dorishui225@gmail.com>
2019-07-08 17:02:21 -0500

    fixed text and image overflow for carousel, added logic to repeat items if less than 12 similar items returned

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import Slider from 'react-slick';
-import '../style.css'
+import React from "react";
+import Slider from "react-slick";
+import "../style.css";
 
-const Carousel = (props) => {
-  console.log(props.items)
-  let twelveItems = props.items.slice(0,12)
-  const itemList = twelveItems.map((item) => {
+const Carousel = props => {
+  let similarItems = props.items;
+  let twelveItems = similarItems.slice(0, 12);
+  const itemList = twelveItems.map(item => {
     return (
-    <div>
-      <img src={item.item_image} height={200}></img>
-      <p>{item.item_name}</p>
-      <h3>$ {item.item_price}</h3>
-    </div>
-    )
-  })
+      <div>
+        <img src={item.item_image} />
+        <p className="item_name">{item.item_name}</p>
+        <h3>$ {item.item_price}</h3>
+        <p>{item.shipping}</p>
+      </div>
+    );
+  });
   var settings = {
     dots: false,
     infinite: false,
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 6,
-    arrows:true
+    arrows: true
   };
   return (
-    <div>
-    <p>Similar sponsored items</p>
-    <Slider {...settings}>
-      {itemList}
-    </Slider>
+    <div className='container'>
+      <p>Similar sponsored items</p>
+      <Slider {...settings}>{itemList}</Slider>
     </div>
   );
-}
+};
 
 export default Carousel;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,32 @@
 // dotenv must be required as early as possible for env vars to propogate
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
-const db = require('./database/db')
+const port = 3001;
+const db = require("./database/db");
 
 app.use(cors());
-app.use(express.static('client/dist'));
+app.use(express.static("client/dist"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.status(200).send('Get request successful!');
+app.get("/", (req, res) => {
+  res.status(200).send("Get request successful!");
 });
 
-app.get('/getItems', (req,res) => {
-  let currItemID = req.query.itemID
-  db.getCurrItemCategory(currItemID,(category)=>{
-    db.getSimilarItems(category,(results) => {
-      res.send(results)
-    })
-  })
-})
+app.get("/getItems", (req, res) => {
+  let currItemID = req.query.itemID;
+  db.getCurrItemCategory(currItemID, category => {
+    db.getSimilarItems(category, results => {
+      if (results.length < 12) {
+        for (let i = 0; i < 12 / results.length; i++) {
+          results.push(...results);
+        }
+      }
+      res.send(results);
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
