c14064ef6b0d7b47e7c3ff4752e0b2a31f062d2a
Doris Hui <dorishui225@gmail.com>
2019-07-12 09:58:59 -0500

    changed aws host

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'http://ec2-18-222-240-36.us-east-2.compute.amazonaws.com/',
+      host: 'ec2-18-222-240-36.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/seabay.pem',
       ref: 'origin/master',
       repo: 'git@github.com:seabay-hratx42-fec/similar-items.git',
