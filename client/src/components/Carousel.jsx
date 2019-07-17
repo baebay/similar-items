6f258e6df110374477132e044d106c85f949c77d
Doris Hui <dorishui225@gmail.com>
2019-07-17 11:10:05 -0500

    made carousel more responsive by showing less items when screen size is reduced

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -31,7 +31,30 @@ const Carousel = props => {
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 6,
-    arrows: true
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
   };
   return (
     <div>
