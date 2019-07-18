e36fe637b83408b8637e9624092af9a091b2cbc3
Doris Hui <dorishui225@gmail.com>
2019-07-18 15:13:25 -0500

    wrote tests for /getItems route

--- test.js
+++ test.js
@@ -14,11 +14,33 @@ describe("Test /getItems", () => {
       done();
     });
   });
-
+  
+  test("Item category should be Sea Books if itemID of 1 is passed to /getItems", done => {
+    request(app).get("/getItems?itemID=1").then(response => {
+      expect(response.body[0].item_category).toBe('Sea Books');
+      done();
+    });
+  });
+  
+  test("Item category should be Toys if itemID of 55 is passed to /getItems", done => {
+    request(app).get("/getItems?itemID=55").then(response => {
+      expect(response.body[0].item_category).toBe('Toys');
+      done();
+    });
+  });
+  
+  test("/getItems should return at least 12 similar items", done => {
+    request(app).get("/getItems?itemID=25").then(response => {
+      expect(response.body.length).toBeGreaterThanOrEqual(12);
+      done();
+    });
+  });
+  
   test("It should send a status code of 404 if itemID is not passed to /getItems", done => {
     request(app).get("/getItems").then(response => {
       expect(response.statusCode).toBe(404);
       done();
     });
   });
+
 });
